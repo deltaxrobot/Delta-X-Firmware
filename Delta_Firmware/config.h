5579071fce7f23ac057037ceb9017d063068658e
Nidhish (ru2saig) <develruusaig@gmail.com>
2023-08-02 19:05:41 +0530

    Using clip to control the custom gripper, for now

--- Delta_Firmware/config.h
+++ Delta_Firmware/config.h
@@ -63,8 +63,8 @@
 
 #define CLIP
 #ifdef CLIP
- #define CLIP_OPEN_ANGLE_SERVO 110
- #define CLIP_CLOSE_ANGLE_SERVO 55
+ #define CLIP_OPEN_ANGLE_SERVO 200
+ #define CLIP_CLOSE_ANGLE_SERVO 80
  #define CLIP_DEG_PER_SECONDS	400 //for RC Servo 9G
 #endif // CLIP
 
