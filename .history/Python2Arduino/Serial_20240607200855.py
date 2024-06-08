11d36c9fb13f1c1950c2eb63d515d7ac38c06a31
Ziller0831 <wesley222666@gmail.com>
2024-06-08 22:05:12 +0800

    <feat> 調整一些Serial.py的內容

---
+++ .history/Python2Arduino/Serial_20240607200855.py
@@ -0,0 +1,38 @@
+import serial
+import time
+import math
+
+ser = serial.Serial('/dev/MEGA2560', 115200, timeout=1)  # open serial port
+time.sleep(2)
+print(ser.readline())
+
+gcodes = []
+
+Circle_r = 100
+Resolution = 2
+
+gcodes.append('G28')
+gcodes.append('G01 F100 Z-450'q)
+gcodes.append('G01 X100')
+gcodes.append('G01 Z-578')
+
+for degree in range(0, 360, Resolution):
+    x = math.cos(math.radians(degree)) * Circle_r
+    y = math.sin(math.radians(degree)) * Circle_r
+    gcodes.append('G01 X{} Y{}'.format(round(x, 2), round(y, 2)))
+
+gcodes.append('G01 Z-450')
+gcodes.append('G01 X0 Y0')
+
+for gcode in gcodes:
+    print(gcode)
+    ser.write((gcode + '\n').encode())
+    while 1:
+        response = ser.readline()
+        print(response)
+        if (response.find('Ok'.encode()) > -1):
+            break
+        elif (response.find('Unknown'.encode()) > -1):
+            break
+
+ser.close()             # close port
